79c09b2d731eca535b95f7edbf1c4613c99d67b7
Kulo Kenci <kulo.kenci@gmail.com>
2019-10-15 13:49:30 +0700

    windl

---
+++ windl/php/get_edition.php
@@ -0,0 +1,58 @@
+<?php
+error_reporting(0);
+//include 'inc/JavaScriptUnpacker_.php';
+$ipclient = $_SERVER['REMOTE_ADDR'] ?: ($_SERVER['HTTP_X_FORWARDED_FOR'] ?: $_SERVER['HTTP_CLIENT_IP']);
+/* gets the data from a URL */
+function qr_loadUrl($url){
+    if (is_callable('curl_init')) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        header("Access-Control-Allow-Origin: *");
+        header("X-Forwarded-For: $ipclient");
+        $data = curl_exec($ch);
+        curl_close($ch);
+    }
+    if (empty($data) || !is_callable('curl_init')) {
+        $opts     = array(
+            'http' => array(
+                'header' => 'Connection: close'
+            )
+        );
+        $context  = stream_context_create($opts);
+        $headers  = get_headers($url);
+        $httpcode = substr($headers[0], 9, 3);
+        if ($httpcode == '200')
+            $data = file_get_contents($url, false, $context);
+        else {
+            $data = '{"div":"Error ' . $httpcode . ': Invalid Url<br />"}';
+        }
+    }
+    return $data;
+}
+function get_data($url)
+{
+    $ch      = curl_init();
+    $timeout = 5;
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    header("Access-Control-Allow-Origin: *");
+    header("X-Forwarded-For: $ipclient");
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
+if($_GET['version_id'] != "" && $_GET['lang'] != ""){
+	$version_id = $_GET['version_id'];
+	$lang = $_GET['lang'];
+	$url = qr_loadUrl('https://tb.rg-adguard.net/php/get_edition.php?version_id='.$version_id.'&lang='.$lang);
+    echo $url;
+} else {
+    $url = qr_loadUrl('https://tb.rg-adguard.net/php/get_edition.php');
+    echo $url;
+}
+?>
